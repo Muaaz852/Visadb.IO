93a474032eb638cc118123c9964d7b53dcf246f6
Muaaz Hamood <gct.165103.a@gmail.com>
2019-01-13 18:04:53 +0500

    Vuetify Added

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary">
+
+        <app-header-item>
+
+            <img src="../../assets/visa-db-brand-logo.png" slot="nav-brand-logo" width="200" height="50">
+
+        </app-header-item>
+
+        <app-header-item>
+
+        </app-header-item>
+
+    </nav>
+</template>
+
+<script>
+    import HeaderItem from './HeaderItem.vue';
+
+    export default {
+        name: "Header",
+
+        components: {
+            appHeaderItem: HeaderItem
+        },
+
+        data() {
+            return {
+                navItems: ['Ask Question', 'Passport Ranking', 'Partner', 'News', 'Contact Us']
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header/HeaderItem.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <slot name="nav-brand-logo">
+
+            <a class="navbar-brand" href="#">
+
+                <slot name="nav-item-brand-logo">
+
+                </slot>
+
+            </a>
+
+        </slot>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+
+                <slot name="nav-simple-item">
+
+                </slot>
+
+            </li>
+        </ul>
+        <!--<slot name="nav-login-button">-->
+
+        <!--</slot>-->
+
+        <!--<slot name="nav-language-select">-->
+
+        <!--</slot>-->
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "HeaderItem",
+    }
+
+</script>
+
+<style scoped>
+    .brand-logo {
+        position: absolute;
+        left: 12px;
+        top: 8px;
+
+        /*background: url(../../assets/visa-db-brand-logo.png);*/
+        opacity: 0.5;
+    }
+</style>
+
+/* image */
+
+position: absolute;
+width: 142.33px;
+height: 35px;
+left: 12px;
+top: 8px;
+
+background: url(image.png);
+opacity: 0.5;
